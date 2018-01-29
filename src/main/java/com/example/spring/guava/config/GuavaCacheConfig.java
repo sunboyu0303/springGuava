6c40d0c660a6e442677db57c1b178230bdf5af8e
sunboyu <sunboyu@jd.com>
2018-01-29 09:25:39 +0800

    create new branch

--- src/main/java/com/example/spring/guava/config/GuavaCacheConfig.java
+++ src/main/java/com/example/spring/guava/config/GuavaCacheConfig.java
@@ -1,13 +1,16 @@
 package com.example.spring.guava.config;
 
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.RemovalListener;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.guava.GuavaCacheManager;
+import org.springframework.cache.guava.GuavaCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -18,10 +21,15 @@ import java.util.concurrent.TimeUnit;
 @EnableCaching
 public class GuavaCacheConfig {
 
+    /**
+     * cache key
+     */
+    private static final String HOTEL_POSTION = "hotel_position";
+
     @Bean
-    public CacheManager cacheManager() {
-        GuavaCacheManager guavaCacheManager = new GuavaCacheManager();
-        guavaCacheManager.setCacheBuilder(CacheBuilder.newBuilder()
+    public Cache defaultCacheBuilder() {
+        Cache cache = CacheBuilder.newBuilder()
+                .recordStats()
                 // 设置大小，条目数
                 .maximumSize(1 << 10)
                 // 设置失效时间，创建时间
@@ -30,7 +38,19 @@ public class GuavaCacheConfig {
                 .expireAfterAccess(20, TimeUnit.HOURS)
                 // 移除监听
                 .removalListener((RemovalListener) removalNotification -> System.out.println("有缓存数据被移除了")
-                ));
-        return guavaCacheManager;
+                ).build();
+        return cache;
+    }
+
+    @Bean
+    public GuavaCache buildHotelPositionCache() {
+        return new GuavaCache(HOTEL_POSTION, defaultCacheBuilder());
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager manager = new SimpleCacheManager();
+        manager.setCaches(Arrays.asList(buildHotelPositionCache()));
+        return manager;
     }
 }
